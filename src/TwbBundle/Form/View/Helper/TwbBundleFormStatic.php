9bf1358206430fbd5758e9b6e8ade3dbba6ff9b2
Nicolas SANDRI (MediaXtend) <contact@mediaxtend.net>
2016-03-30 14:19:47 +0200

    Added support for form static element:  - tag name ;  - class ;  - attributes.

--- src/TwbBundle/Form/View/Helper/TwbBundleFormStatic.php
+++ src/TwbBundle/Form/View/Helper/TwbBundleFormStatic.php
@@ -2,14 +2,19 @@
 namespace TwbBundle\Form\View\Helper;
 
 use Zend\Form\ElementInterface;
-use Zend\View\Helper\AbstractHelper;
+use Zend\Form\View\Helper\AbstractHelper;
 
 class TwbBundleFormStatic extends AbstractHelper
 {
     /**
      * @var string
      */
-    private static $staticFormat = '<p class="form-control-static">%s</p>';
+    private static $staticFormat = '<%s class="form-control-static%s"%s>%s</%s>';
+
+    /**
+     * @var array
+     */
+     private static $ignoredAttributes = ['type', 'tag', 'class'];
 
     /**
      * Invoke helper as functor
@@ -35,6 +40,62 @@ class TwbBundleFormStatic extends AbstractHelper
      */
     public function render(ElementInterface $oElement)
     {
-        return sprintf(self::$staticFormat, $oElement->getValue());
+        $sTag = $this->getTagFromElement($oElement);
+        return $this->renderElement($sTag, $oElement->getValue(), $this->getClassFromElement($oElement), $this->getAttributesFromElement($oElement));
+    }
+
+    /**
+     * @param ElementInterface $oElement
+     * @return string
+     */
+    public function getTagFromElement(\Zend\Form\ElementInterface $oElement)
+    {
+        $sTag = $oElement->getAttribute('tag');
+        return ($sTag && preg_match("/^[a-z][a-z0-9._-]*$/", $sTag) ? $sTag : 'p');
+    }
+
+   /**
+     * @param ElementInterface $oElement
+     * @return string
+     */
+    public function getClassFromElement(\Zend\Form\ElementInterface $oElement)
+    {
+        $sClass = $oElement->getAttribute('class');
+        return ($sClass ? ' ' . $sClass : '');
+    }
+
+    /**
+     * @param ElementInterface $oElement
+     * @return string
+     */
+    public function getAttributesFromElement(\Zend\Form\ElementInterface $oElement)
+    {
+        $aAttributes = $oElement->getAttributes();
+        return is_array($aAttributes) ? ' ' . $this->createAttributesString(array_diff_key($aAttributes, array_flip(self::$ignoredAttributes))) : '';
+    }
+
+    /**
+     * @param string $sTag
+     * @param string $sElementContent
+     * @param string $sRowClass
+     * @param string $sElementAttributes
+     * @return string
+     * @throws \InvalidArgumentException
+     */
+    public function renderElement($sTag, $sElementContent, $sRowClass, $sElementAttributes)
+    {
+        if (!is_string($sTag)) {
+            throw new \InvalidArgumentException('Argument "$sTag" expects a string, "' . (is_object($sTag) ? get_class($sTag) : gettype($sTag)) . '" given');
+        }
+        if (!is_string($sElementContent)) {
+            throw new \InvalidArgumentException('Argument "$sElementContent" expects a string, "' . (is_object($sElementContent) ? get_class($sElementContent) : gettype($sElementContent)) . '" given');
+        }
+        if (!is_string($sRowClass)) {
+            throw new \InvalidArgumentException('Argument "$sRowClass" expects a string, "' . (is_object($sRowClass) ? get_class($sRowClass) : gettype($sRowClass)) . '" given');
+        }
+        if (!is_string($sElementAttributes)) {
+            throw new \InvalidArgumentException('Argument "$sElementAttributes" expects a string, "' . (is_object($sElementAttributes) ? get_class($sElementAttributes) : gettype($sElementAttributes)) . '" given');
+        }
+        return sprintf(self::$staticFormat, $sTag, $sRowClass, $sElementAttributes, $sElementContent, $sTag) . PHP_EOL;
     }
 }
